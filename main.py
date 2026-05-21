db484b398762ac1781d3a7d0f50505609dc2057d
mijan182 <jana-marie.reyes@rcsd.ca>
2026-05-21 15:09:44 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#this is the main file where everything is called in!
+
+import EYES2026
+import goldfinger
+import time
+import sys
+
+s = "Muhahahahaha! I have infiltrated your code and messed it all up!\nYou can't catch me because I USED AI to cover my TRACKS HAHAHAHAHA \n\n\t-"
+# clue 1 put in here
+
+for c in s:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+
+#
