01a4eec8af2dbd55db8e2c312cb29d8bc9a86d10
mijan182 <jana-marie.reyes@rcsd.ca>
2026-05-29 12:00:56 -0600

    update: new stuff

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 # this is the main file where everything is called in!
 
 import EYES2026
-import goldfinger
 import EYESextra
 
 
+#??????
+EYESextra.nothing()
+
+#enter
+
+
 def clue1():
-  print("How many letters are in EYES?")
-  x = int(input())
-  EYESextra.clueans1(x)
+    print("How many letters are in EYES?")
+    x = int(input())
+    EYESextra.clueans1(x)
+
 
 def clue2():
+    print("What color is the box?")
+    y = str(input())
 
-  print("What color is the box?")
-  y = str(input())
-  
-  if y == EYESextra.secret_color:
-      EYESextra.clueans2()
-  else:
-      print("WRONG! Try again!")
+    if y == EYESextra.secret_color:
+        EYESextra.clueans2()
+    else:
+        print("WRONG! Try again!")
+
 
 def clue3():
     number1 = 10
@@ -34,24 +40,23 @@ def clue4():
     print("\n Make the cat meow three times!\n")
     EYESextra.cat()
     print("\n")
-    
+
     meowCount = 3
-    
+
     while meowCount > 0:
         user_input = input("Press Enter to make the cat meow: ")
-        if user_input.lower() == 'meow()' or user_input == '':
+        if user_input.lower() == "meow()" or user_input == "":
             EYESextra.meow()
             meowCount -= 1
             print(f"{meowCount} meows remaining!")
-    
+
     if meowCount == 0:
         EYESextra.clueans4()
 
+
 def finalclue():
     userinp = input("What coding language are we using right now?:\n")
-    if userinp.lower() == 'python':
+    if userinp.lower() == "python":
         EYESextra.finalAnswer()
     else:
         print("run this clue one more time to try again!")
-
-  
